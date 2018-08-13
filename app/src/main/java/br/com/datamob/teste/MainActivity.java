2c595614657d41c9165bbd7f0db4bd0829c034dd
Rafael Sarreta Lucindo <rafael.lucindo@baraodemaua.br>
2018-08-13 20:43:51 -0300

    Parcial

--- app/src/main/java/br/com/datamob/teste/MainActivity.java
+++ app/src/main/java/br/com/datamob/teste/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
-        Main.main();
+        new Main().main();
     }
 }
--- app/src/main/java/br/com/datamob/teste/testejava/Main.java
+++ app/src/main/java/br/com/datamob/teste/testejava/Main.java
@@ -1,40 +1,7 @@
 package br.com.datamob.teste.testejava;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.Display;
 import android.view.View;
-import android.widget.Button;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 
 public class Main implements View.OnClickListener
@@ -52,21 +19,6 @@ public class Main implements View.OnClickListener
         {
             redeSocial.getEnderecoPerfil();
         }
-
-
-        twitters.get(0).getFeed();
-
-        Button button = new Button(new Context()
-        {
-        });
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                clickouNoBotao();
-            }
-        });
     }
 
     private static void addFacebook(Facebook facebook)
